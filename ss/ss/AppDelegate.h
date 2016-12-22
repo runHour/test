73a86be36e97dface042a170afdeff57e6f37400
lanrenzhoumo <https://github.com/runHour/lanrenzhoumoDemo>
2016-12-22 12:43:18 +0800

    first

---
+++ ss/ss/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ss
+//
+//  Created by Ibokan on 2016/12/22.
+//  Copyright © 2016年 fuyuzheng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ss/ss/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ss
+//
+//  Created by Ibokan on 2016/12/22.
+//  Copyright © 2016年 fuyuzheng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
